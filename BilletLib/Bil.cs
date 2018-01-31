78dde9990bb1ce62edc9263b877af337521b7050
Hassan Ali Mohdi <hm98-@hotmail.dk>
2018-01-31 14:19:10 +0100

    opgave 2 done

---
+++ BilletLib/Bil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib
+{
+    public class Bil
+    {
+        private string _nummerplade;
+        private DateTime _dato;
+
+
+        public string Nummerplade
+        {
+            get { return this._nummerplade; }
+            set { _nummerplade = value; }
+        }
+
+        public DateTime Dato
+        {
+            get { return this._dato; }
+            set { _dato = value; }
+        }
+
+
+
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string køretøj()
+        {
+            return "Bil";
+        }
+
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Bil_Pris()
+        {
+            Bil bil= new Bil();
+            Assert.AreEqual(bil.Pris(),240);
+        }
+
+        [TestMethod]
+        public void Bil_Køretøj()
+        {
+            Bil køretøj = new Bil();
+            Assert.AreEqual(køretøj.køretøj(), "Bil");
+        }
+    }
+}
